9b6bc61835afd126728065d840d01783578da977
Alan Hu <on.alanhu@gmail.com>
2019-04-05 13:38:57 +0200

    style(lex): deployment

---
+++ server/build/precache-manifest.983dbcedf756c3666d0cb7961ab108a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "71163cf0c24fc2312103",
+    "url": "/static/css/main.c291ebf6.chunk.css"
+  },
+  {
+    "revision": "71163cf0c24fc2312103",
+    "url": "/static/js/main.71163cf0.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "63dc951ceeac886173f0",
+    "url": "/static/js/2.63dc951c.chunk.js"
+  },
+  {
+    "revision": "f5ef4263a9c36adac59559031d67a0b7",
+    "url": "/static/media/blank-profile.f5ef4263.png"
+  },
+  {
+    "revision": "67bab102fbb736f2293c567485b0f84d",
+    "url": "/static/media/logo.67bab102.png"
+  },
+  {
+    "revision": "baac4f0ed969ea20c5b3787e074eab20",
+    "url": "/index.html"
+  }
+];
