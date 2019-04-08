9df5b265a4ff80c8c278d292eaaec8000ec79896
Alan Hu <on.alanhu@gmail.com>
2019-04-08 13:48:56 +0200

    fix(server routes): small fix to dashboard route

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,7 +9,7 @@ router.get('/auth', ctrl.users.logIn);
 router.get('/', (req, res) => res.sendFile(path.join('/index.html')));
 router.get('/login', (req, res) => res.redirect('/'));
 router.get('/register', (req, res) => res.redirect('/'));
-router.get('/dashboard/*', (req, res) => res.redirect('/'));
+router.get('/dashboard*', (req, res) => res.redirect('/'));
 
 router.use(authentication, protectedRoutes);
 
