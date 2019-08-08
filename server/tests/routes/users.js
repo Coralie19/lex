4efe14bdac75ab6e8fece55dd78d0f7f412b6c8a
RobAntunes <robm.antunes@gmail.com>
2019-08-08 10:06:50 +0200

    Enhanced User Tests

--- server/tests/routes/users.js
+++ server/tests/routes/users.js
@@ -9,6 +9,23 @@ module.exports = (app) => {
   const username = 'buzz';
   const password = 'buzz';
 
+  //Unit Testing
+
+  describe('Valid endpoint access', () => {
+    it('Should redirect from home to login && return correct status code',
+      () => chai.request(app)
+        .get('/')
+        .then((res) => {
+          expect(res).to.have.status(200);
+          expect('Location', '/login');
+        })
+        .catch((err) => {
+          throw err;
+        }))
+  });
+
+  //Integration Testing
+
   describe('User lifecycle', () => {
     let token;
     it('should correctly create a new user', () => chai.request(app)
@@ -32,14 +49,30 @@ module.exports = (app) => {
         throw err;
       }));
 
-    it('should successfully log a user in and return a token', () => chai.request(app)
-      .get('/login')
-      .set('Authorization', `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`)
+    it(`should redirect to edit profile page when user is successfully logged
+        in and return status code 201`,
+      () => chai.request(app)
+        .get('/login')
+        .set('Authorization', `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`)
+        .then((res) => {
+          console.log(res);
+          expect(res).to.have.status(200);
+          expect(res.body).to.have.property('token');
+          expect(res.body.token).to.be.a('string');
+          token = res.body.token;
+          expect('Location', '/dashboard/edit');
+        })
+        .catch((err) => {
+          throw err;
+        }));
+
+    it('should redirect to the edit profile page upon creating a new user', () => chai.request(app)
+      .post('/register')
+      .send({ username: 'Binge', password: 'Drinking' })
       .then((res) => {
-        expect(res).to.have.status(200);
-        expect(res.body).to.have.property('token');
-        expect(res.body.token).to.be.a('string');
-        token = res.body.token;
+        expect(res).to.have.status(201);
+        expect(res.body.user.username).to.eql('Binge');
+        expect('Location', '/dashboard/edit');
       })
       .catch((err) => {
         throw err;
@@ -59,28 +92,44 @@ module.exports = (app) => {
       .get('/login')
       .set('Authorization', `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`)
       .then((res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('token');
+        expect(res.body.token).to.be.a('string');
+        token = res.body.token;
+      })
+      .catch((err) => {
+        throw err;
+      }))
 
+    it('Should block requests without authorization', () => chai.request(app)
+      .get('/users/id/5')
+      .then((res) => {
+        expect(res).to.have.status(401);
       })
       .catch((err) => {
         throw err;
       }));
+
+    it('GET /users/id/:id should route correctly', () => chai.request(app)
+      .get('/users/username/5')
+      .then((res) => {
+        expect(res).to.have.status(401);
+      })
+      .catch((err) => {
+        throw err;
+      }));
+
+
+    it('should not have stored token on home page (after user logout)', () => chai.request(app)
+      .get('/')
+      .then((res) => {
+        console.log(res);
+        expect(res).to.have.status(200);
+        expect(res.body.token).to.eql(undefined);
+      })
+      .catch((err) => {
+        throw err;
+      })
+    );
   });
-
-  it('Should block requests without authorization', () => chai.request(app)
-    .get('/users/id/5')
-    .then((res) => {
-      expect(res).to.have.status(401);
-    })
-    .catch((err) => {
-      throw err;
-    }));
-
-  it('GET /users/id/:id should route correctly', () => chai.request(app)
-    .get('/users/username/5')
-    .then((res) => {
-      expect(res).to.have.status(401);
-    })
-    .catch((err) => {
-      throw err;
-    }));
-};
+}
